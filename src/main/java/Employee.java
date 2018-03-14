3dd3456801c2711acc2f1d2ce20ea27cbfb5077d
Aayushi <aayushi3@illinois.edu>
2018-03-14 16:29:41 -0500

    lab8 complete

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -4,14 +4,16 @@
  * @see <a href="https://cs125.cs.illinois.edu/lab/10/">Lab 10 Description</a>
  */
 public class Employee {
-
+    /** name */
     private String name;
+
+    /** manager */
     private String manager;
 
     /**
      * Constructor for initialization.
-     * @param name
-     * @param manager
+     * @param name name
+     * @param manager manager
      */
     public Employee(final String name, final String manager) {
         this.name = name;
@@ -28,7 +30,7 @@ public class Employee {
 
     /**
      * Setter for name.
-     * @param name
+     * @param name name
      */
     public void setName(final String name) {
         this.name = name;
@@ -44,9 +46,75 @@ public class Employee {
 
     /**
      * Setter for manager.
-     * @param manager
+     * @param manager manager
      */
     public void setManager(final String manager) {
         this.manager = manager;
     }
+
+    /** list of employees. */
+    private java.util.List<Employee> employees;
+
+    /**
+     * Constructor which initializes the employees list.
+     */
+    public EmployeeDatabase() {
+        
+    }
+
+    /**
+     * Returns the manager for the given employee.
+     * @param employee the employee to return the manager for
+     * @return the employee's manager
+     */
+    public Employee findManager(final Employee employee) {
+        String managerName = employee.getManager();
+        for (int i = 0; i < employees.size(); i++) {
+            Employee temp = employees.get(i);
+            if (managerName.equals(temp.toString())) {
+                return employees.get(i);
+            }
+        }
+    }
+
+    /**
+     * Count the number of managers above this employee.
+     * @param employee the name of the employee
+     * @return the number of managers above
+     */
+    public int countManagersAbove(final Employee employee) {
+        int count = 0;
+        Employee temp = findManager(employee);
+        while (temp != null) {
+            count++;
+            temp = findManager(temp);
+        }
+        return count;
+    }
+
+    /**
+     * Count the number of employees under this manager.
+     * @param employee name of the employee
+     * @return number of employees under this manager
+     */
+    public int countEmployeesUnder(final Employee employee) {
+        int counter = 0;
+        for (int i = 0; i < employees.size(); i++) {
+            Employee temp = employees.get(i);
+            temp = temp.findManager();
+            if (temp.equals(employee)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    /**
+     * Main method for testing.
+     * @param unused unused input arguments
+     */
+    public static void main(java.lang.String[] unused) {
+
+    }
+
 }
